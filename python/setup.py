9a5c390213f0eee825872adc5c47e1bbaa45de79
brentnash <brenash@amazon.com>
2016-03-12 08:03:36 -0800

    Remove old generated package files and add license to all Python files

--- python/setup.py
+++ python/setup.py
@@ -1,3 +1,16 @@
+# Copyright 2014, Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Amazon Software License (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+# http://aws.amazon.com/asl/
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
 from setuptools import setup
 import os.path
 
@@ -9,14 +22,14 @@ def read_file(filename):
 setup(
   name = 'aws_kpl_agg',
   packages = ['aws_kpl_agg'],
-  version = '1.0.10',
-  description = 'Python module to simplify processing of Amazon Kinesis Records which have been created with the Kinesis Producer Library.',
+  version = '1.0.0',
+  description = 'Python module to assist in taking advantage of the Kinesis Producer Library aggregated record format for both aggregation and deaggregation.',
   long_description=read_file('README.rst'),
   author = 'Brent Nash',
   author_email = 'brenash@amazon.com',
   license = 'SEE LICENSE IN LICENSE.TXT',
-  url = 'http://github.com/awslabs/kinesis-deaggregation',
-  keywords = ['aws','kinesis','deaggregation'],
+  url = 'http://github.com/awslabs/kinesis-aggregation',
+  keywords = ['aws','kinesis','aggregation'],
   classifiers = ['Development Status :: 5 - Production/Stable',
                 'Intended Audience :: Developers',
                  'License :: Other/Proprietary License',
