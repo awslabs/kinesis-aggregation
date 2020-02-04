f6f579a782a6a05e9330b9076bcf6579a1887bf8
Andrew Jorgensen <ajorgens@amazon.com>
2020-02-04 09:11:07 -0800

    Relicense to Apache-2.0

--- node/test/aggdeagg.js
+++ node/test/aggdeagg.js
@@ -1,3 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 const assert = require('assert')
 const sinon = require('sinon');
 const should = require('should')
