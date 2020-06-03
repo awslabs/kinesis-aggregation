ee0d49a5ba5b8c54203acbcc5d15d2af5cf4273f
IanMeyers <meyersi@amazon.com>
2020-06-03 17:27:00 +0100

    Version 1.1.0
    Updated to commons-lang v3
    Updated java-lambda-events version to 3.10, which changes the class hierarchy.
    Added unit tests for updates to deaggregation lambda functions

--- java/KinesisAggregator/src/main/java/com/amazonaws/kinesis/agg/AggRecord.java
+++ java/KinesisAggregator/src/main/java/com/amazonaws/kinesis/agg/AggRecord.java
@@ -28,7 +28,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import com.amazonaws.annotation.NotThreadSafe;
 import com.amazonaws.services.kinesis.clientlibrary.types.Messages.AggregatedRecord;
