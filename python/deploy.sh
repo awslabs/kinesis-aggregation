9cca767a49f5af36f89e6bf0d83d78705f52dda5
Ian Meyers <meyersi@amazon.com>
2022-06-10 11:43:49 +0100

    Version 1.2.2
    Adding root directory to setup

--- python/deploy.sh
+++ python/deploy.sh
@@ -3,8 +3,6 @@
 if [ "$1" == "build" ]; then
   rm -Rf dist build && python3 setup.py sdist bdist_wheel
   twine check dist/*
-
-  python3 make_lambda_build.py
 elif [ "$1" == "deploy-test" ]; then
   twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 elif [ "$1" == "deploy-prod" ]; then
